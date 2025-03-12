6f2bb6bb7a93690adeb9ef702894678b6109c819
nishant6666 <158477065+nishant6666@users.noreply.github.com>
2025-03-12 19:18:44 +0530

    server isuues fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-import http from "http"
-import app from './app.js'
+const http = require("http");
+const app = require("./app.js");
+
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app) ;
